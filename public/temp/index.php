782a70f7413ffd5da01dacfb2f8b420353c5dd00
milostoplicic <38977214+milostoplicic@users.noreply.github.com>
2018-08-31 19:44:50 +0200

    Create index.php

---
+++ public/temp/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Protection
+if (!defined('ENTER')){die('Denied access!');}
+
+?>
+<!doctype html>
+<html lang='{%html_lang%}'>
+<head>
+	<meta charset='{%charset%}'>
+	<meta name='description' content='{%description%}'>
+	<meta name='keywords' content='{%keywords%}'>
+	<meta name='author' content='{%author%}'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<link rel='icon' type='image/png' href='{%favicon%}'>
+	<link rel='stylesheet' type='text/css' href='{%style%}'>
+	<title>{%title%}</title>
+</head>
+
+<body>
+{%body%}
+</body>
+</html>
