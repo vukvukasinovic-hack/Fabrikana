8504b6b8a1b2a09ae069a323d9f31a7b44d7dc1c
milostoplicic <38977214+milostoplicic@users.noreply.github.com>
2018-09-01 20:30:42 +0200

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -5,5 +5,15 @@ if (!defined('ENTER')){die('Denied access!');}
 
 $c = array(
 
-	'copyright'		=> "Copyright 2018. <b>Fabrikana</b> GPL License"
+	'copyright'		=> "Copyright 2018. <b>Fabrikana</b> GPL License",
+	'key'			=> 'Key',
+	'signin'		=> 'Sign In',
+	'signup'		=> 'Sign Up',
+	'forgot'		=> 'Forgot data?',
+	'blog'			=> 'Blog',
+	'lang'			=> 'Language/Letter',
+	'en'			=> 'English',
+	'cir'			=> 'Cyrillic',
+	'lat'			=> 'Latin',
+	'fabrikana'		=> 'Fabrikana'
 );
