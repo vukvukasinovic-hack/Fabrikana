ea7c4a243ba0492c9d4818d0984a0f4fe30918e9
milostoplicic <38977214+milostoplicic@users.noreply.github.com>
2018-09-01 12:26:02 +0200

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,9 @@
+<?php
+
+// Protection
+if (!defined('ENTER')){die('Denied access!');}
+
+$c = array(
+
+	'copyright'		=> "Copyright 2018. <b>Fabrikana</b> GPL License"
+);
---
+++ lang/sr.php
@@ -0,0 +1,9 @@
+<?php
+
+// Protection
+if (!defined('ENTER')){die('Denied access!');}
+
+$c = array(
+
+	'copyright'		=> "Ауторска права 2018. <b>Фабрикана</b> ГПЛ Лиценца"
+);
