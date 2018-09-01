493c49562a88b8c25c08c56c3a2638d71ec432c8
milostoplicic <38977214+milostoplicic@users.noreply.github.com>
2018-09-01 20:27:45 +0200

    Update Content.class.php

--- engine/classes/Content.class.php
+++ engine/classes/Content.class.php
@@ -19,11 +19,43 @@ class Content {
 	}
 
 	static private function Menu() {
+		
+		$c1 = (C1 == '') ? '' : '/'.C1;
+		$c2 = (C2 == '') ? '' : '/'.C2;
+		$c3 = (C3 == '') ? '' : '/'.C3;
+		$c4 = (C4 == '') ? '' : '/'.C4;
+		$c5 = (C5 == '') ? '' : '/'.C5;
+		
+		if (LANG === 'en') {
 
+			require 'lang/en.php';
+		} else if (LANG === 'cir' OR LANG === 'lat') {
+
+			require 'lang/sr.php';
+		}
+
+		if ($_SESSION[SITE]['usertype'] == 0) {
+
+			require_once 'public/temp/menu0.php';
+		} else if ($_SESSION[SITE]['usertype'] == 1) {
+
+			require_once 'public/temp/menu1.php';
+		} else if ($_SESSION[SITE]['usertype'] == 2) {
+
+			require_once 'public/temp/menu2.php';
+		} else if ($_SESSION[SITE]['usertype'] == 3) {
+
+			require_once 'public/temp/menu3.php';
+		} else if ($_SESSION[SITE]['usertype'] == 4) {
+
+			require_once 'public/temp/menu4.php';
+		}
+
+		return $menu;
 	}
 
 	static private function Main() {
-
+		
 	}
 
 	static private function Right() {
